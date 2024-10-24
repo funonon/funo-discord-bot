4ab8c0a3044ecd71b3a813bf72280e5df7e205b5
funonon <funo222@outlook.jp>
2024-10-24 18:43:16 +0900

    ver1 main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run('YOUR_DISCORD_BOT_TOKEN')
